0a1955391502829b5e2435c1e872485c932c4c19
castilloglenn <castilloglenn@ymail.com>
2024-08-16 14:10:02 +0800

    feat: add source automation script

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# This script sources all other scripts in the same directory
+current_path=$(pwd)
+echo "Sourcing scripts in $current_path\n"
+current_file=$(basename "$0")
+
+sh_files=($(dirname "$0")/functions/*.sh)
+total_files=$((${#sh_files[@]} - 1))
+index=1
+
+for file in "${sh_files[@]}"; do
+    if [ "$(basename "$file")" != "$current_file" ]; then
+        echo "=== File $index of $total_files ==="
+        echo "Sourcing $(basename "$file")"
+
+        # Fetch function names from the file
+        function_names=$(grep -E '^[a-zA-Z_][a-zA-Z0-9_]*\s*\(\s*\)\s*\{' "$file" | awk '{print $1}' | tr '\n' ',' | sed 's/,$//')
+
+        # Print function names as a comma-separated list
+        if [ -n "$function_names" ]; then
+            echo "Functions: $(echo $function_names | paste -sd ', ' -)\n"
+        else
+            echo "No functions in $(basename "$file").\n"
+        fi
+
+        source "$file"
+        ((index++))
+    fi
+done
