1b94df592d964bd71a96c14bfa2a199009a3b604
castilloglenn <castilloglenn@ymail.com>
2024-09-26 11:40:36 +0800

    refactor: new branch format

--- functions/git.sh
+++ functions/git.sh
@@ -1,9 +1,9 @@
 cs_newbranch() {
-    if [ -z "$1" ] || [ -z "$2" ]; then
-        echo "Usage: nb <azure-boards-number> <branch-name>"
+    if [ -z "$1" ]; then
+        echo "Usage: cs_newbranch <branch-name>"
         return 1
     fi
-    git checkout -b castilloglenn/$1/$2
+    git checkout -b castilloglenn/$1
 }
 
 cs_rebasemain() {
@@ -12,16 +12,6 @@ cs_rebasemain() {
     git fetch origin --prune
     git rebase origin/main
     git stash pop
-}
-
-cs_pushtonewbranch() {
-    if [ -z "$1" ]; then
-        echo "Usage: pnb <branch-name>"
-        return 1
-    fi
-    DATE=$(date +"%Y/%m/%d")
-    git checkout -b castilloglenn/$DATE/$1
-    git push --set-upstream origin castilloglenn/$DATE/$1
 }
 
 cs_switchaccount() {
