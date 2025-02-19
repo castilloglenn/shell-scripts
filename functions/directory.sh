f4876e84af990eb223e7edf1b722092c84773bed
dev-glenn-lr <dev_glenn@lrtechs.co.jp>
2025-02-19 17:21:46 +0800

    feat: add code editor launch for project navigation functions

--- functions/directory.sh
+++ functions/directory.sh
@@ -5,17 +5,21 @@ goto() {
 
 tw() {
     cd "$HOME${CLIENT_DIRECTORY}takokase-web"
+    code .
 }
 
 tm() {
     cd "$HOME${CLIENT_DIRECTORY}takokase-mobile"
+    code .
 }
 
 tb() {
     cd "$HOME${CLIENT_DIRECTORY}takokase-backend"
+    code .
 }
 
 ts() {
     cd "$HOME${CLIENT_DIRECTORY}takokase-scripts"
+    code .
 }
 
