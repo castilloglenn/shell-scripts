0ad3cf9632eaeb8866dc7e24393ceb71c720be42
castilloglenn <castilloglenn@ymail.com>
2026-01-13 08:48:50 +0800

    feat: add snake_to_pascal function for converting snake_case to PascalCase

---
+++ functions/strings.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# Convert snake_case (or kebab-case) to PascalCase
+# Usage: snake_to_pascal "some_value"  OR  echo "some_value" | snake_to_pascal
+snake_to_pascal() {
+    local s
+    if [ $# -eq 0 ]; then
+        IFS= read -r s || return 0
+    else
+        s="$1"
+    fi
+
+    # normalize separators, trim edge underscores
+    s="${s//-/_}"
+    s="${s##_}"
+    s="${s%%_}"
+    [ -z "$s" ] && printf '' && return 0
+
+    # replace underscores with spaces, capitalize words, join
+    printf '%s' "$s" | tr '_' ' ' | awk '{
+        for (i=1;i<=NF;i++) {
+            printf "%s", toupper(substr($i,1,1)) tolower(substr($i,2))
+        }
+    }'
+    printf '\n'
+}
