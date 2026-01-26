9fb7ec3a8b027ed3c7a1f0290e8a41432f4cfa96
castilloglenn <castilloglenn@ymail.com>
2026-01-26 21:42:32 +0800

    feat: add adbconnect function for connecting to devices via ADB

--- functions/flutter.sh
+++ functions/flutter.sh
@@ -10,3 +10,12 @@ cs_reinstallflutterdeps() {
     cd ..
     flutter pub run build_runner build --delete-conflicting-outputs
 }
+
+adbconnect() {
+    if [ $# -ne 3 ]; then
+        echo "Usage: adbconnect <third_octet> <fourth_octet> <port>"
+        return 1
+    fi
+    addr="192.168.$1.$2:$3"
+    adb connect "$addr"
+}
